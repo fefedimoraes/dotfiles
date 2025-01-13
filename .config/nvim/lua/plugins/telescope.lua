68987b49e19e91c8ef39b0d4549df12c821a7d52
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-01-12 22:19:25 -0600

    Fix Telescope find files and live grep

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,16 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+        pickers = {
+            find_files = {
+                find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' }
+            },
+            live_grep = {
+                additional_args = { '--hidden', '--glob', '!**/.git/*' }
+            }
+        }
+    }
 }
 
