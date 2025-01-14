27aae9945124e2ced547df71d9d91aaaf6c7f48b
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-01-14 01:12:16 -0600

    Add closing braces and quotes remaps

--- .config/nvim/lua/fefedimoraes/remap.lua
+++ .config/nvim/lua/fefedimoraes/remap.lua
@@ -3,3 +3,9 @@ vim.g.maplocalleader = '\\'
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
+vim.keymap.set('i', '"', '""<left>')
+vim.keymap.set('i', "'", "''<left>")
+vim.keymap.set('i', '(', '()<left>')
+vim.keymap.set('i', '[', '[]<left>')
+vim.keymap.set('i', '{', '{}<left>')
+
