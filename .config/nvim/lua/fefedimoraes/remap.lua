45e0f579c6bf0b154f2965a4fcbbe691de3c5477
Fernando Moraes <moraesf@amazon.com>
2025-01-14 21:22:05 -0600

    Add remaps for Yank / Paste

--- .config/nvim/lua/fefedimoraes/remap.lua
+++ .config/nvim/lua/fefedimoraes/remap.lua
@@ -9,3 +9,13 @@ vim.keymap.set('i', '(', '()<left>')
 vim.keymap.set('i', '[', '[]<left>')
 vim.keymap.set('i', '{', '{}<left>')
 
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+yg_')
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>yy', ' "+yy')
+
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('v', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>P', '"+P')
+
