021644ead744951ab94452d715ed5806ba3ff00b
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-03-03 19:31:56 -0600

    Add ui-select + Telescope notify keymap
    With UI Select, neovim core stuff can fill the telescope picker. An
    example would be `lua vim.lsp.buf.code_action()`.

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local telescope = require('telescope')
+local themes = require('telescope.themes')
 local telescopeBuiltin = require('telescope.builtin')
 
 telescope.setup({
@@ -7,11 +8,15 @@ telescope.setup({
             hijack_netrw = true,
             hidden = { file_browser = true, folder_browser = true },
         },
+        ['ui-select'] = {
+            themes.get_cursor {}
+        }
     },
 })
 
 telescope.load_extension('file_browser')
 telescope.load_extension('notify')
+telescope.load_extension('ui-select')
 
 vim.keymap.set('n', '<leader>fe', telescope.extensions.file_browser.file_browser, { desc = 'Telescope file explorer' })
 vim.keymap.set('n', '<space>fE', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', { desc = 'Telescope file explorer (current buffer directory)' })
@@ -19,5 +24,5 @@ vim.keymap.set('n', '<leader>ff', telescopeBuiltin.find_files, { desc = 'Telesco
 vim.keymap.set('n', '<leader>fg', telescopeBuiltin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', telescopeBuiltin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', telescopeBuiltin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>fn', function () telescope.extensions.notify.notify() end, { desc = 'Telescope notifications' })
+vim.keymap.set('n', '<leader>fn', function () telescope.extensions.notify.notify(themes.get_dropdown {}) end, { desc = 'Telescope notifications' })
 
---
+++ .config/nvim/lua/plugins/telescope-ui-select.lua
@@ -0,0 +1,7 @@
+return {
+    'nvim-telescope/telescope-ui-select.nvim',
+    dependencies = {
+        'nvim-telescope/telescope.nvim',
+        'nvim-lua/plenary.nvim',
+    }
+}
