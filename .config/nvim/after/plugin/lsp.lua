3b66a872c047a51c9febf65817b36d341821a9af
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-03-16 21:22:46 -0500

    Add noice, trouble and cmp-cmdline

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -81,3 +81,26 @@ cmp.setup({
         end,
     },
 })
+
+-- `/` cmdline setup.
+cmp.setup.cmdline("/", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "buffer" },
+    },
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" },
+    }, {
+        {
+            name = "cmdline",
+            option = {
+                ignore_cmds = { "Man", "!" },
+            },
+        },
+    }),
+})
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,5 +4,5 @@ return {
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-cmdline'},
 }
-
