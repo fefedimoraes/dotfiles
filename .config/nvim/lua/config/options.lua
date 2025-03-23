ed83300e470ee9dc5e8039e1da0b25f5c6490329
Fernando Moraes <moraesf@amazon.com>
2025-03-22 20:45:14 -0500

    Minor tweaks

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Show whitespaces
+vim.opt.list = true
+vim.opt.listchars:append({
+  space = "·",
+  tab = "▷ ",
+  trail = "~",
+  extends = "◣",
+  precedes = "◢",
+  nbsp = "␣",
+})
---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,36 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add tsx and treesitter
+    vim.list_extend(opts.ensure_installed, {
+      "bash",
+      "c",
+      "css",
+      "csv",
+      "diff",
+      "editorconfig",
+      "gitignore",
+      "gnuplot",
+      "html",
+      "http",
+      "java",
+      "javascript",
+      "json",
+      "kotlin",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "sql",
+      "tmux",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    })
+  end,
+}
