bd53643edc1007ea64f0173638a5546fe3d3d7f1
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-03-26 00:35:01 -0500

    Config changes to sketchybar

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install font for Sketchybar
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.5/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# Install SbarLua
+git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/
