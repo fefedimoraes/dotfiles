94274f2de9b30fc0a053e74bee2f95b400255d3e
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-06-27 02:41:33 -0500

    Add checkbox rendering to render-markdown

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -4,5 +4,24 @@ return {
     code = {
       sign = true,
     },
+    checkbox = {
+      enabled = true,
+      render_modes = false,
+      bullet = false,
+      right_pad = 1,
+      unchecked = {
+        icon = "󰄱 ",
+        highlight = "RenderMarkdownUnchecked",
+        scope_highlight = nil,
+      },
+      checked = {
+        icon = "󰱒 ",
+        highlight = "RenderMarkdownChecked",
+        scope_highlight = nil,
+      },
+      custom = {
+        todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+      },
+    },
   },
 }
