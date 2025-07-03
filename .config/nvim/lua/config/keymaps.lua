cf7f9cc4d2036ecc9c620557d7c60098cac2fee4
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-07-03 10:23:30 -0500

    Remove { } keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,8 +10,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz") -- when moving down a page
 vim.keymap.set("n", "<C-u>", "<C-u>zz") -- when moving up a page
 vim.keymap.set("n", "n", "nzzzv") -- when moving to the next search match
 vim.keymap.set("n", "N", "Nzzzv") -- when moving to the previous search match
-vim.keymap.set("n", "{", "{zz") -- when moving up a block
-vim.keymap.set("n", "}", "}zz") -- when moving down a block
+-- vim.keymap.set("n", "{", "{zz") -- when moving up a block
+-- vim.keymap.set("n", "}", "}zz") -- when moving down a block
 
 -- Replace hovered over word
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
