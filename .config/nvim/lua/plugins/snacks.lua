37db4074744b9af37fd36dfe753989aa7eaf8388
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-07-12 14:41:23 -0500

    Add neovim logo in Snacks Dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -7,6 +7,26 @@ return {
           files = { hidden = true },
         },
       },
+      dashboard = {
+        sections = {
+          {
+            section = "terminal",
+            align = "center",
+            cmd = "cat ~/.config/nvim/neovim-logo.cat",
+            height = 22,
+            indent = 12,
+            padding = 1,
+          },
+          {
+            section = "keys",
+            gap = 1,
+            padding = 1,
+          },
+          {
+            section = "startup",
+          },
+        },
+      },
     },
   },
 }
