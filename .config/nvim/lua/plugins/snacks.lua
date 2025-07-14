d47fbf5c643a13ec6e137d8c4675d5f3ad78b9c4
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-07-14 15:36:43 -0500

    Remove Recent Files and Projects section from dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -23,20 +23,6 @@ return {
             padding = 1,
           },
           {
-            icon = " ",
-            title = "Recent Files",
-            section = "recent_files",
-            indent = 2,
-            padding = 1,
-          },
-          {
-            icon = " ",
-            title = "Projects",
-            section = "projects",
-            indent = 2,
-            padding = 1,
-          },
-          {
             icon = " ",
             title = "Git Status",
             section = "terminal",
