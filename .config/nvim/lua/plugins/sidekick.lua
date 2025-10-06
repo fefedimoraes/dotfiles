288dc2208868886531fa2c70996753982fe27bfa
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-10-06 08:14:11 -0700

    Update nvim plugins / Add sidekick

---
+++ .config/nvim/lua/plugins/sidekick.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/sidekick.nvim",
+  opts = {
+    -- add any options here
+    cli = {
+      mux = {
+        backend = "tmux",
+        enabled = true,
+      },
+    },
+    nes = {
+      enabled = false,
+    },
+  },
+}
