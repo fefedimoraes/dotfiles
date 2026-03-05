ca2bb89ce214d4b29e6e877bb8c4ab61812c07ad
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2026-03-05 13:49:44 -0800

    Add up to HEAD~10 diff view keymaps

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,32 +1,37 @@
+local keys = {
+  {
+    "<leader>dv",
+    function()
+      if next(require("diffview.lib").views) == nil then
+        vim.cmd("DiffviewOpen")
+      else
+        vim.cmd("DiffviewClose")
+      end
+    end,
+    desc = "Toggle Diffview window",
+  },
+}
+
+for i = 1, 10 do
+  keys[#keys + 1] = {
+    "<leader>d" .. (i % 10),
+    function()
+      if next(require("diffview.lib").views) == nil then
+        vim.cmd("DiffviewOpen HEAD~" .. i)
+      else
+        vim.cmd("DiffviewClose")
+      end
+    end,
+    desc = "Toggle Diffview HEAD~" .. i,
+  }
+end
+
 return {
   {
     "sindrets/diffview.nvim",
     dependencies = {
       { "nvim-tree/nvim-web-devicons", lazy = true },
     },
-    keys = {
-      {
-        "<leader>dv",
-        function()
-          if next(require("diffview.lib").views) == nil then
-            vim.cmd("DiffviewOpen")
-          else
-            vim.cmd("DiffviewClose")
-          end
-        end,
-        desc = "Toggle Diffview window",
-      },
-      {
-        "<leader>d1",
-        function()
-          if next(require("diffview.lib").views) == nil then
-            vim.cmd("DiffviewOpen HEAD~1")
-          else
-            vim.cmd("DiffviewClose")
-          end
-        end,
-        desc = "Toggle Diffview window",
-      },
-    },
+    keys = keys,
   },
 }
