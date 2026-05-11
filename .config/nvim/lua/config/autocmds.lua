c1d61b57d4e6017f864ab3aae9d1110e32e7b3fa
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2026-05-10 23:22:02 -0700

    Enable nvim progress bars on tmux

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -11,3 +11,22 @@ vim.api.nvim_create_user_command("W", "w", { nargs = 0 }) -- Remap :W to :w
 vim.api.nvim_create_user_command("Wa", "wa", { nargs = 0 }) -- Remap :Wa to :wa
 vim.api.nvim_create_user_command("Q", "q", { nargs = 0 }) -- Remap :Q to :q
 vim.api.nvim_create_user_command("Qa", "qa", { nargs = 0 }) -- Remap :Qa to :qa
+
+if os.getenv("TMUX") then
+  --- wraps message with tmux prefix so that the underlying terminal can interpret it correctly
+  --- needs 'set-option -g allow-passthrough on' in tmux config
+  ---@param content string
+  ---@return string
+  local function wrap_tmux(content)
+    return string.format("\27Ptmux;\27%s\27\\", content)
+  end
+
+  local original_ui_send = vim.api.nvim_ui_send
+
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.api.nvim_ui_send =
+    ---@param content string
+    function(content)
+      original_ui_send(wrap_tmux(content))
+    end
+end
